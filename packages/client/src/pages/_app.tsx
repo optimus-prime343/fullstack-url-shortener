5c25efd49d5f7ba36dddb51c80bdefed687502ff
Sachin Yo <sachin_aryal>
2023-02-05 18:29:54 +0545

    Added mantine as UI library on client

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '@/styles/globals.css'
 
+import type { MantineProviderProps } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = useMemo<MantineProviderProps['theme']>(
+    () => ({
+      primaryColor: 'indigo',
+      colorScheme: 'dark',
+    }),
+    []
+  )
+  return (
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
