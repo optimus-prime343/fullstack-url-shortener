a56da2b757048c699ed16806651a57b03f45675c
Sachin Yo <sachin_aryal>
2023-02-07 11:51:36 +0545

    Replaced NextJS with solid on client side

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js'
+
+const App: Component = () => {
+  return <div>Hello From SolidJS</div>
+}
+
+export default App
--- packages/client/src/constants/modal-keys.ts
+++
@@ -1,3 +0,0 @@
-export const modalKeys = {
-  getStartedModal: 'get-started-modal',
-} as const
